642314b579932c730b5de2a4c769c73c8d2cca49
Reyarmenta <reynaldo.armenta18@tectijuana.edu.mx>
2021-08-12 23:11:42 -0700

    Avance 12/8/2021 Reprak Links v1

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 
-const MediaCard = ({name,logo}) => {
+const MediaCard = ({name,logo, classLogo, urlSocial}) => {
+    const cardStyle = `media-card ${classLogo}`
     return( 
-        <div className="media-card">
+        <a href={urlSocial} target="_blank" className={cardStyle}>
             <h1 className="card-title">{name}</h1>
             <img className="card-logo" src={logo} alt={name} />
             <button className="card-button">Follow Me</button>
-        </div>
+        </a>
     )
 }
 
--- src/containers/SocialMedia.jsx
+++ src/containers/SocialMedia.jsx
@@ -4,18 +4,18 @@ import MediaCard from '../components/MediaCard'
 
 import logoGithub from '../assets/github-logo.svg'
 import logoInstagram from '../assets/instagram-logo.svg'
-import logoTiktok from '../assets/tiktok-logo.svg'
+import logoTiktok from '../assets/tiktok-logo3.svg'
 import logoTwitch from '../assets/twitch-logo.svg'
 import logoTwitter from '../assets/twitter-logo.svg'
 
 const SocialMedia = () => {
     return( 
         <div className="social-media">
-            <MediaCard name="github" logo={logoGithub} />
-            <MediaCard name="instagram" logo={logoInstagram} />
-            <MediaCard name="tiktok" logo={logoTiktok} />
-            <MediaCard name="twitch" logo={logoTwitch} />
-            <MediaCard name="twitter" logo={logoTwitter} />
+            <MediaCard name="github" urlSocial='https://github.com/Reprak11' logo={logoGithub} classLogo='gitLogo'/>
+            <MediaCard name="instagram" urlSocial='https://www.instagram.com/reprak_11/' logo={logoInstagram} classLogo='insLogo'/>
+            <MediaCard name="tiktok" urlSocial='https://www.tiktok.com/@reprak11' logo={logoTiktok} classLogo='tikLogo'/>
+            <MediaCard name="twitch" urlSocial='https://www.twitch.tv/reprak11' logo={logoTwitch} classLogo='twitchLogo'/>
+            <MediaCard name="twitter" urlSocial='https://twitter.com/reprak11' logo={logoTwitter} classLogo='twitterLogo'/>
         </div>
     )
 }
